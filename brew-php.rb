829ae92aa8d1a758cb076941443f1c0ebc278ab9
Erin Millard <ezzatron@gmail.com>
2018-04-05 14:34:08 +1000

    Updated to 1.1.0.

--- brew-php.rb
+++ brew-php.rb
@@ -2,9 +2,9 @@ class BrewPhp < Formula
   desc "Manage multiple PHP installations"
   homepage "https://github.com/ezzatron/brew-php"
 
-  version "1.0.0"
-  url "https://github.com/ezzatron/brew-php/releases/download/1.0.0/brew-php.tar.gz"
-  sha256 "6656ddaf05910404c56eac6d2949a6b949ce2757d770bf2411f90e8e1b1f3bb5"
+  version "1.1.0"
+  url "https://github.com/ezzatron/brew-php/releases/download/1.1.0/brew-php.tar.gz"
+  sha256 "eae96030a77e8497eecdcee6e33b158bbe3b962435532ba803cf26fd6d9efe4f"
 
   def install
       bin.install "brew-php"
